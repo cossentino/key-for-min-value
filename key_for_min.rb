9ac83e9b653ba1d198b0a504518d765b89363b25
Ian Cossentino <icossentino@gmail.com>
2020-11-10 20:30:07 -0500

    commit tree 7ce457a6cb73df319b66e27c3ee659decc1260ac

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_value_key = ""
+    min_value = 1000000000
+    if name_hash == {}
+        return nil
+    else
+        name_hash.each do |key, value|
+            if name_hash[key] < min_value
+                min_value = name_hash[key]
+                min_value_key = key
+            else
+                next
+            end
+        end
+    end
+    min_value_key
 end
